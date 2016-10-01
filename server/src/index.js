2a9fa2542eddd9db7b88a3d1321e93880a190c64
maccorzo <marko@hankkila.me>
2016-10-01 08:47:32 +0300

    move express app into separate file to prepare it for testing

--- server/src/index.js
+++ server/src/index.js
@@ -1,29 +1,15 @@
-// npm packages
+// import our packages
 
-import express from 'express';
-
-// init app
-var app = express();
-
-// test method
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-// catch all unhandler errors
-app.use( function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send(err);
-});
+import app from './app';
 
 // start server
 app.listen(8080, function() {
+  
   const host = this.address().address;
   const port = this.address().port;
   console.log(`Shard listening at http://${host}:${port}`);
 });
 
 // output all uncaught exceptions
-process.on('uncaughtException', function err() { console.error('uncaught exception:', err)});
-process.on('unhandledRejection', function error() { console.error('unhandled rejection:', error)});
+process.on('uncaughtException', err => console.error('uncaught exception:', err));
+process.on('unhandledRejection', error => console.error('unhandled rejection:', error));
